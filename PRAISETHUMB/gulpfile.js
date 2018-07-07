f508327348ec263460a7af2da8a4dfac750ecaa9
System Administrator <root@wangzemindeMacBook-Pro.local>
2018-07-08 00:50:24 +0800

    key

--- PRAISETHUMB/gulpfile.js
+++ PRAISETHUMB/gulpfile.js
@@ -1,10 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-gulp.task('default', ['praise'],function () {
-    gulp.watch(['src/**/*.es6',"!src/public/*.es6"],['praise'])
-});
 
-gulp.task('praise', function () {
+gulp.task('default', function () {
     gulp.src(['src/**/*.es6',"!src/public/*/*.es6"])
         .pipe(babel({
             presets: ['env']
